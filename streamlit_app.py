f2da1598e82bfa96d6eab041eb2feea4900b808f
MasayaDake <noimpslmtbrk@gmail.com>
2023-07-31 00:21:41 +0900

    main関数の抽出と生成ファイルを.gitignoreへ￥

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import streamlit as st
 import openai
 import asyncio
 
-# ChatGPT APIのエンドポイント
-chatgpt_url = "https://api.openai.com/v1/chat/completions"
-# ChatGPT APIのトークン読み込み/GPT-3.5 TurboのAPIキーを設定
-openai.api_key = st.secrets["chatgpt_api_key"]
 
 async def get_audio_query():
     # audio_queryコマンドを実行
@@ -23,24 +19,35 @@ async def get_audio_query():
     with open("audio.wav", "wb") as file:
         file.write(response.stdout)
 
-# Streamlitアプリのタイトルとテキスト入力
-st.title("Voicevox Core with ChatGPT Streamlit Demo For AIIT System Programming Report 4")
-text_input = st.text_area("Enter a prompt:", value="AIITの大喜利をして20文字以内", key="text_input")  # keyを追加
 
-if st.button("Generate Voice", key="generate_button"):  # keyを追加
-    # ChatGPTによるテキスト生成
-    messages = [{"role": "user", "content": text_input + " 10文字以上50文字以内で答えよ"}]
-    st.write("Messages:", messages)  # 追加
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-    response_data = completion["choices"][0]["message"]["content"].strip()
-    st.write("Response Data:", response_data)  # 追加
+def main():
+    # ChatGPT APIのエンドポイント
+    chatgpt_url = "https://api.openai.com/v1/chat/completions"
+    # ChatGPT APIのトークン読み込み/GPT-3.5 TurboのAPIキーを設定
+    openai.api_key = st.secrets["chatgpt_api_key"]
 
-    # テキストをファイルに書き出す
-    with open("text.txt", "w", encoding="utf-8") as f:
-        f.write(response_data)
+    # Streamlitアプリのタイトルとテキスト入力
+    st.title("Voicevox Core with ChatGPT Streamlit Demo For AIIT System Programming Report 4")
+    text_input = st.text_area("Enter a prompt:", value="AIITの大喜利をして", key="text_input")  # keyを追加
 
-    # 非同期処理を実行する
-    st.spinner("音声生成中...")
-    asyncio.run(get_audio_query())
-    st.success("音声生成が完了しました！")
-    st.audio("audio.wav", format="audio/wav")
+    if st.button("Generate Voice", key="generate_button"):  # keyを追加
+        # ChatGPTによるテキスト生成
+        messages = [{"role": "user", "content": text_input + " 10文字以上50文字以内で答えよ"}]
+        st.write("Messages:", messages)  # 追加
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        response_data = completion["choices"][0]["message"]["content"].strip()
+        st.write("Response Data:", response_data)  # 追加
+
+        # テキストをファイルに書き出す
+        with open("text.txt", "w", encoding="utf-8") as f:
+            f.write(response_data)
+
+        # 非同期処理を実行する
+        st.spinner("音声生成中...")
+        asyncio.run(get_audio_query())
+        st.success("音声生成が完了しました！")
+        st.audio("audio.wav", format="audio/wav")
+
+
+if __name__ == "__main__":
+    main()
