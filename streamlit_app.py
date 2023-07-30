c195b40964f2917be7dfcaf58cab8c7fd813ad94
MasayaDake <noimpslmtbrk@gmail.com>
2023-07-31 00:48:57 +0900

    画像生成も追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,15 @@ speakers = {
     }
 }
 
-async def get_audio_query(speaker_id):
 
+def generate_image(prompt):
+    # 画像生成のAPIを呼び出して画像を生成する
+    response = openai.Image.create(prompt=prompt, n=1, size="512x512")
+    image_url = response["data"][0]["url"]
+    return image_url
+
+
+async def get_audio_query(speaker_id):
     # audio_queryコマンドを実行
     audio_query_cmd = f'curl -s -X POST "localhost:50021/audio_query?speaker={speaker_id}" --get --data-urlencode ' \
                       f'text@text.txt > query.json '
@@ -69,15 +76,15 @@ def main():
     openai.api_key = st.secrets["chatgpt_api_key"]
 
     # Streamlitアプリのタイトルとテキスト入力
-    st.title("Voicevox Core with ChatGPT Streamlit Demo For AIIT System Programming Report 4")
-    text_input = st.text_area("Enter a prompt:", value="AIITの大喜利をして", key="text_input")  # keyを追加
+    st.title("大喜利お題アプリ")
+    text_input = st.text_area("大喜利のお題", value="大喜利のお題を入れて ex) AIITで3ヶ月に1度しか提供されない学食のメニューとは", key="text_input")  # keyを追加
 
     # 話者を選択するドロップダウンメニュー
-    selected_speaker = st.selectbox("Select a speaker:", list(speakers.keys()))
+    selected_speaker = st.selectbox("お題に答えてくれる人", list(speakers.keys()))
 
-    if st.button("Generate Voice", key="generate_button"):  # keyを追加
+    if st.button("AIが回答するよ", key="generate_button"):  # keyを追加
         # ChatGPTによるテキスト生成
-        messages = [{"role": "user", "content": text_input + " 10文字以上50文字以内で答えよ"}]
+        messages = [{"role": "user", "content": text_input + "という大喜利に答えて。10文字以上50文字以内で答えよ"}]
         st.write("Messages:", messages)  # 追加
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
         response_data = completion["choices"][0]["message"]["content"].strip()
@@ -86,6 +93,12 @@ def main():
         # テキストをファイルに書き出す
         with open("text.txt", "w", encoding="utf-8") as f:
             f.write(response_data)
+
+        # 画像を生成する
+        st.spinner("画像生成中...")
+        image_url = generate_image(response_data + "というお題の画像")
+        st.success("画像生成が完了しました！")
+        st.image(image_url, caption="Generated Image", use_column_width=True)
 
         # 非同期処理を実行する
         st.spinner("音声生成中...")
